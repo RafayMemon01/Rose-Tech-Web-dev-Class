891b31ea3ff85383d17bf79927065a303c6ce908
Rafay Memon <rafayrashid200611@gmail.com>
2024-05-18 18:36:57 +0500

    js varibles class

--- ES6/script.js
+++ ES6/script.js
@@ -1,6 +1,21 @@
-const name = "Amanullah";
-let fathername = "Amjad";
-let group = "Science";
+let num1 = 10; //10
+
+num1= num1 + 12; // 22
+
+num1 = 0  //0
+
+let num2 = 12; //12
+
+let num3 = 12; //12
+
+num3=5 //5
+
+console.log(num1+num2+num3);
 
 
-console.log(name);
+let Name = "Ziyan";
+
+let name ="aman"
+
+console.log(name)
+
