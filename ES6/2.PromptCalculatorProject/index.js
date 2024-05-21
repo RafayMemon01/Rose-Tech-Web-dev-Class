c6683c5e50feaebdbae38c88b185a556cc3832aa
Rafay Memon <rafayrashid200611@gmail.com>
2024-05-21 18:08:57 +0500

    push

---
+++ ES6/2.PromptCalculatorProject/index.js
@@ -0,0 +1,12 @@
+// let num1 = prompt("write your first Number");
+// let num2 = prompt("write your scond number here");
+
+// num1 = Number(num1)
+// num2 = Number(num2)
+
+// let result = num1+num2;  //5
+
+// alert(result)
+
+
+
---
+++ ES6/3.Unfamiliar operator/script.js
@@ -0,0 +1,10 @@
+let number1 = 2;
+
+let number2 = number1+1
+
+
+console.log(number1++)
+console.log(number1++)
+console.log(number1++)
+console.log(number1++)
+
