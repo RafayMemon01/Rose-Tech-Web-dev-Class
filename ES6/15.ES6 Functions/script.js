a5f3d7037ca21402082c4409af2ec8ed60fb2e79
Rafay Memon <rafayrashid200611@gmail.com>
2024-06-06 18:37:07 +0500

    Es6 fun

---
+++ ES6/15.ES6 Functions/script.js
@@ -0,0 +1,26 @@
+// function myFunction() {
+//   alert("Hello World");
+//   }
+  
+let hello = "false"
+
+const AdditionFunc = (a,b) => {
+    let result = a + b;
+    alert(result)
+    return result;
+};
+
+AdditionFunc(2, 3);
+
+const mySecondFun = () => {
+    // console.log(hello)
+}
+
+
+// window.addEventListener("load", myFun)
+
+
+
+
+// myFun()
+// mySecondFun()
