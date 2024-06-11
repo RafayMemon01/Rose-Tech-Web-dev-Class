1505bbbed8d9a3d939f7fe2815f7021142cd1020
Rafay Memon <rafayrashid200611@gmail.com>
2024-06-11 18:21:12 +0500

    Hello World Update

--- ES6/1.JSHelloworld/script.js
+++ ES6/1.JSHelloworld/script.js
@@ -1,21 +1,76 @@
-let num1 = 10; //10
+// let num1 = 10; //10
 
-num1= num1 + 12; // 22
+// num1= num1 + 12; // 22
 
-num1 = 0  //0
+// num1 = 0  //0
 
-let num2 = 12; //12
+// let num2 = 12; //12
 
-let num3 = 12; //12
+// let num3 = 12; //12
 
-num3=5 //5
+// num3=5 //5
 
-console.log(num1+num2+num3);
+// console.log(num1+num2+num3);
 
 
-let Name = "Ziyan";
+// let Name = "Ziyan";
 
-let name ="aman"
+// let name ="aman"
 
-console.log(name)
+// console.log(name)
+
+
+
+// There Are 3 Types Of Varible
+
+// let
+// const
+// var (depreciated)
+
+let num1 = 10;    //Number type
+
+
+let name = "Zain Ahmad" //String type
+
+let isMale = true; //Boolean type
+
+// alert(isMale)
+
+
+let secondName = "Ahmad";
+
+name = "Ahtisham"
+
+const Name = "Zain Ahmad"
+
+// Name = "Ahtisham"
+
+console.log(Name)
+
+let number1 = 2; //2
+
+let num3 = number1; //2
+
+let result = num3 + number1;  //2 + //2
+
+console.log(result)
+
+
+
+
+
+let num4 = 5;
+let num5= 7;
+
+result=num4+num5;
+
+
+let a = 2;
+let b = 2;
+alert();
+
+
+
+
+
 
