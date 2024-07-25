086871426c5012d7ab99c48ef9951a02f9e9ac43
Rafay Memon <rafayrashid200611@gmail.com>
2024-07-25 19:09:41 +0500

    code

---
+++ React JS/01reacthelloworld/src/App.js
@@ -0,0 +1,13 @@
+import MyComp from './MyComp';
+import './style.css'
+function App() {
+  // let name = 'ziyan'
+  return (
+    <>
+      <h1 className="hello">Hello World React</h1>
+      <MyComp name="amanullah" />
+    </>
+  );
+}
+
+export default App;
---
+++ React JS/01reacthelloworld/src/MyComp.js
@@ -0,0 +1,10 @@
+function MyComp({name}) {
+    return(
+        <>
+        <p>Hello i am PARagraph Component</p>
+        <h2>{name}</h2>
+        </>
+    )
+}
+
+export default MyComp;
